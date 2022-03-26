61142bfecff72bdb568286aa96445a35dd6067cd
AJmal <23mixchannel@gmail.com>
2022-03-26 16:49:49 +0500

    unused provider has been delated

--- lib/provider/FormProvider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:crm/service/boxService.dart';
-import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-
-class FormProvider extends ChangeNotifier {
-  final key = GlobalKey<FormState>();
-}
